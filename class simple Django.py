318ef351c02d5cf36cfc8af7b2367af1bbbb0242
Franck da COSTA <franckivond@gmail.com>
2022-11-21 22:10:00 +0100

    Update class simple Django.py

--- class simple Django.py
+++ class simple Django.py
@@ -1,5 +1,7 @@
 from django.db import models
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Sujet(models.Model) :
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    last_modified = models.DateTimeField(auto_now_add = True)
+    img = models.ImageField(upload_to = "images/")
